014c010ae103d2c9f08b07254b6e4914770e11b2
marcellinuselbert11 <marcellinuselbert46@gmail.com>
2020-06-23 16:41:47 +0700

    tes

--- src/main.js
+++ src/main.js
@@ -1,9 +1,20 @@
 /* eslint-disable */
 import Vue from 'vue';
 import ShardsVue from 'shards-vue';
+import VCalendar from 'v-calendar';
+Vue.use(VCalendar, {
+  componentPrefix: 'vc',  // Use <vc-calendar /> instead of <v-calendar />
+              // ...other defaults
+});
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
+// Install BootstrapVue
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
 
 // Styles
 import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
 import '@/scss/shards-dashboards.scss';
 import '@/assets/scss/date-range.scss';
 
